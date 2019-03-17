0a00ce0d48125e1f622ada917608826282ee9294
Mariana Garman <garman.mariana@gmail.com>
2019-03-17 19:38:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,6 +16,6 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees). each { |attendee| puts (attendees)}
-  puts assign_rooms(attendees).each { |attendee| puts (room)}
+  puts assign_rooms(attendees).each { |attendee| puts (attendees)}
 end
 
